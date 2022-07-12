5bd30f06cb3fd35c04b39e2393ac0abc8b0d54ea
konst <13412775+konst-github@users.noreply.github.com>
2022-07-12 21:10:36 +0200

    Test files and polishes

--- most_common/main.c
+++ most_common/main.c
@@ -30,6 +30,7 @@
 
 FILE * open_file(const char file_path[]);
 LONG file_size(FILE * file);
+void compare_files(FILE * file1, FILE * file2);
 
 
 char * cmd_gen = "gen";
@@ -37,14 +38,7 @@ char * cmd_gen = "gen";
 
 int main(int argc, const char *argv[])
 {
-
-	// 1. Check if we've received a correct number of arguments
-
-	if (argc != REQUIRED_ARGC_GEN && argc != REQUIRED_ARGC_COMPARE)
-	{
-		print_usage();
-		return RUN_RESULT_INVALID;
-	}
+	// 0. Output received command line arguments
 
 	printf("\nCommand-line arguments:\n");
 	for(int n = 0; n < argc; n++)
@@ -54,41 +48,51 @@ int main(int argc, const char *argv[])
 	printf("\n");
 
 
-	// 2. Check if 'gen' command execution requested
+	// 1. Check if we've received a correct number of arguments
 
-	int result = strcmp(argv[1], cmd_gen);
-
-	if(result == CMP_RESULT_EQUAL)
+	if (argc == REQUIRED_ARGC_GEN)
 	{
-		printf("Received 'gen' command ->\n\n");
-		printf("Starting test files generation\n\n");
+		// 2. Check if 'gen' command execution requested
+		if(strcmp(argv[1], cmd_gen) == CMP_RESULT_EQUAL)
+		{
+			printf("Received 'gen' command ->\n\n");
+			printf("Starting test files generation\n\n");
 
-		generate_files();
+			generate_files();
 
-		return RUN_RESULT_GEN_SUCCESS;
+			return RUN_RESULT_GEN_SUCCESS;
+		}
+		else
+		{
+			print_usage();
+			return RUN_RESULT_INVALID;
+		}
 	}
-
-
-	// 3. Try to open file for reading
-
-	FILE * file1 = open_file(argv[1]);
-	if(file1 == NULL)
+	else if(argc == REQUIRED_ARGC_COMPARE)
 	{
+		// 3. Try to open file for reading
+
+		FILE * file1 = open_file(argv[1]);
+		if(file1 == NULL)
+		{
+			return RUN_RESULT_INVALID;
+		}
+
+		FILE * file2 = open_file(argv[2]);
+		if(file2 == NULL)
+		{
+			return RUN_RESULT_INVALID;
+		}
+
+		// 4. Start reading file and converting string
+		compare_files(file1, file2);
+
+		return RUN_RESULT_SUCCESS;
+	}
+	else {
+		print_usage();
 		return RUN_RESULT_INVALID;
 	}
-
-	FILE * file2 = open_file(argv[2]);
-	if(file2 == NULL)
-	{
-		return RUN_RESULT_INVALID;
-	}
-
-	// 4. Start reading file and converting string
-	LONG size1 = file_size(file1);
-	LONG size2 = file_size(file2);
-
-
-	return RUN_RESULT_SUCCESS;
 }
 
 
@@ -116,4 +120,21 @@ LONG file_size(FILE * file)
 	fseek(file, 0 , SEEK_SET); // needed for the next read from the beginning of file
 	printf("File size is %lu bytes\n\n", size);
 	return size;
+}
+
+
+void compare_files(FILE * file1, FILE * file2)
+{
+	LONG size1 = file_size(file1);
+	LONG size2 = file_size(file2);
+
+	time_t time_start, time_end;
+
+	time(&time_start);
+	printf("COMPARING FILES START: size1 = %lu, size2 = %lu, time = %lu", size1, size2, time_start);
+
+
+
+	time(&time_end);
+	printf("COMPARING FILES END: time = %lu, elapsed = %f", time_end, difftime(time_end - time_start));
 }
